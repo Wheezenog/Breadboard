472f7215a3a7528a6cf10f56ff72cff1dd33e235
Henry Mullin <hdmullin1057@gmail.com>
2026-02-09 08:19:05 -0500

    Styling updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { DropdownMenu } from 'bits-ui';
 
 	let { children } = $props();
 </script>
@@ -8,5 +9,43 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<header>
+	<div class="flex justify-between p-6 bg-orange-400">
+		<h1 class="text-3xl font-bold text-white"><a href="/">BreadBoard</a></h1>
+
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+					<g fill="none" stroke="#fff" stroke-width="1.5">
+						<circle cx="12" cy="9" r="3" />
+						<path
+							stroke-linecap="round"
+							d="M17.97 20c-.16-2.892-1.045-5-5.97-5s-5.81 2.108-5.97 5"
+						/>
+						<path
+							stroke-linecap="round"
+							d="M7 3.338A9.95 9.95 0 0 1 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12c0-1.821.487-3.53 1.338-5"
+						/>
+					</g>
+				</svg>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Portal>
+				<DropdownMenu.Content
+					class="flex flex-col border-muted bg-white shadow-popover outline-hidden focus-visible:outline-hidden w-[229px] rounded-xl border px-4 py-2 gap-2"
+					sideOffset={8}
+				>
+					<DropdownMenu.Item class="flex items-center gap-2"><span class="icon-[solar--user-linear]"></span>View Profile</DropdownMenu.Item>
+					<DropdownMenu.Item class="flex items-center gap-2"
+						><span class="icon-[solar--settings-linear]"></span>Settings</DropdownMenu.Item
+					>
+					<DropdownMenu.Item class="flex items-center gap-2"
+						><span class="icon-[solar--logout-2-outline]"></span>Log Out</DropdownMenu.Item
+					></DropdownMenu.Content
+				>
+			</DropdownMenu.Portal>
+		</DropdownMenu.Root>
+	</div>
+</header>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import data from '$lib/assets/MockData.json';
+	import data from '$lib/MockData.json';
 	import { Avatar, RatingGroup } from 'bits-ui';
 </script>
 
-<div>
+<div class="flex flex-col items-center">
 	{#each data.reviews as review}
 		<div class="min-w-sm max-w-lg rounded-xl border-1 border-slate-500 p-4 m-8 shadow-md">
-			<div class="flex flex-direction-row items-center py-2 ">
+			<div class="flex flexrow items-center py-2">
 				<Avatar.Root class="mr-2">
 					<div>
 						<Avatar.Fallback
-							><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+							><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
 								<g fill="none" stroke="#060606" stroke-width="1.5">
 									<circle cx="12" cy="9" r="3" />
 									<path
@@ -26,33 +26,27 @@
 						>
 					</div>
 				</Avatar.Root>
-				<p>
-					{data.users.find((u) => u.id === review.user_id)?.username}
-				</p>
+				<div class="flex flex-col">
+					<p class="text-base">{data.users.find((u) => u.id === review.user_id)?.username}</p>
+					<div class="flex items-center flex-row py-2">
+						<span class="icon-[solar--map-point-bold] mr-1 text-xs"></span>
+						<p class="text-xs">
+							{review.location}
+						</p>
+					</div>
+				</div>
 			</div>
+			{#each { length: 5 }, i}
+				{#if i <= review.rating}
+					<span class="icon-[solar--star-bold] text-[#ffa500]"></span>
+				{:else}
+					<span class="icon-[solar--star-outline] text-[#ffa500]"></span>
+				{/if}
+			{/each}
 			<h1 class="font-bold">{review.title}</h1>
-			<RatingGroup.Root value={review.rating} max={5} class="flex py-2">
-				{#snippet children({ items })}
-					{#each items as item (item.index)}
-						<RatingGroup.Item index={item.index}>
-							{#if item.state === 'active'}
-								<span class="icon-[solar--star-bold]"></span>
-							{:else}
-								<span class="icon-[solar--star-outline]"></span>
-							{/if}
-						</RatingGroup.Item>
-					{/each}
-				{/snippet}
-			</RatingGroup.Root>
-			<div class="flex items-center flex-direction-row py-2">
-				<span class="icon-[solar--map-point-bold] mr-2"></span>
-        <p>
-				{review.location}
-        </p>
-			</div>
-      <p class="">
-			{review.content}
-      </p>
+			<p class="">
+				{review.content}
+			</p>
 		</div>
 	{/each}
 </div>
