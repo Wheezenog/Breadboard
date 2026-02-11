de3cf3f57fba4452a80a09396dd85533266842c3
Henry Mullin <hdmullin1057@gmail.com>
2026-02-11 09:00:26 -0500

    Lists tables

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,15 @@
+use aws_config::load_from_env;
+use aws_sdk_dynamodb::config;
+
 pub mod auth;
 pub mod types;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let shared_config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+    let client = Client::new(&shared_config);
+
+    let tables = client.list_tables().send().await?;
+    println!("{:?}", tables.table_names);
+    Ok(())
 }
